9132d15e39b3ec08bb1f9f700f7efa67bf8db2a5
HorseT <horset@gmail.com>
2020-03-29 20:54:50 +0200

    Create persistence context for demo

--- src/main/java/com/martychen/demo/jpa/Demo.java
+++ src/main/java/com/martychen/demo/jpa/Demo.java
@@ -1,7 +1,17 @@
 package com.martychen.demo.jpa;
 
+import javax.persistence.Persistence;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Demo is started");
+
+        createPersistenceContext();
+
+        System.out.println("Demo is ended");
+    }
+
+    private static void createPersistenceContext() {
+        Persistence.createEntityManagerFactory("my-persistence-unit");
     }
 }
