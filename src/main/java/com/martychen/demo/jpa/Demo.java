f401889525164ab1e513e13e74f050a99aefd75c
HorseT <horset@gmail.com>
2020-03-29 20:56:36 +0200

    Add demo for persisting and finding entity instance

--- src/main/java/com/martychen/demo/jpa/Demo.java
+++ src/main/java/com/martychen/demo/jpa/Demo.java
@@ -1,17 +1,46 @@
 package com.martychen.demo.jpa;
 
+import com.martychen.demo.jpa.persistence.entities.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Demo {
+    private static EntityManagerFactory entityManagerFactory;
+
     public static void main(String[] args) {
         System.out.println("Demo is started");
 
         createPersistenceContext();
 
+        Book book = new Book();
+        book.setId(1);
+        book.setTitle("Hello World");
+        book.setDescription("It is always the first application!");
+
+        persistBook(book);
+        showPersistedBookWithId(book.getId());
+
         System.out.println("Demo is ended");
     }
 
     private static void createPersistenceContext() {
-        Persistence.createEntityManagerFactory("my-persistence-unit");
+        entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+    }
+
+    private static void persistBook(Book book) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(book);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    private static void showPersistedBookWithId(long id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Book result = entityManager.find(Book.class, id);
+        System.out.println(result);
+        entityManager.close();
     }
 }
---
+++ src/main/java/com/martychen/demo/jpa/persistence/entities/Book.java
@@ -0,0 +1,45 @@
+package com.martychen.demo.jpa.persistence.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    private long id;
+    private String title;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
